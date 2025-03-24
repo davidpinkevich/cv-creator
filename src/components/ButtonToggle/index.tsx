298974169758e67b16d9d57b7d1cadfefcf2009e
David Pinkevich <davidpinkevich95@gmail.com>
2025-03-24 17:48:02 +0300

    feat: add button toggle

---
+++ src/components/ButtonToggle/index.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+
+export function ButtonToggle() {
+  const [alignment, setAlignment] = useState("ru");
+  const { i18n } = useTranslation();
+
+  const handleLanguageChange = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const handleChange = (
+    _: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    if (alignment !== newAlignment && alignment && newAlignment) {
+      setAlignment(newAlignment);
+      handleLanguageChange(newAlignment);
+    }
+  };
+
+  return (
+    <ToggleButtonGroup
+      color="secondary"
+      value={alignment}
+      exclusive
+      onChange={handleChange}
+      sx={{ position: "absolute", top: "20px", right: "20px" }}
+    >
+      <ToggleButton sx={{ width: "80px" }} value="en">
+        En
+      </ToggleButton>
+      <ToggleButton sx={{ width: "80px" }} value="ru">
+        Ru
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+}
